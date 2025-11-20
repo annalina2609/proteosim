2cd7dc33f14eff13fad2d4e052284ae6878a4cfb
annalina2609 <annalina.baumueller@stud.uni-heidelberg.de>
2025-11-20 15:50:01 +0100

    Add file_handling.py implementation and tests

--- tests/test_file_handling.py
+++ tests/test_file_handling.py
@@ -1,7 +1,7 @@
 from proteosim.file_handling import read_fasta
 
 def test_read_fasta():
-    tmp_fasta_path = '../data/dummy_proteins.fasta'
+    tmp_fasta_path = '../proteosim/data/dummy_proteins.fasta'
     protein_map = read_fasta(tmp_fasta_path)
     
     assert protein_map['P11802'] == "MAT"
